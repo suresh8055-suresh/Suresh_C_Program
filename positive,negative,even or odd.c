70576b32723a2e9cc37c103fe86abb82c2502e59
suresh8055-suresh <sureshkumark8055@gmail.com>
2024-08-31 13:22:10 +0530

    Create positive,negative,even or odd.c

---
+++ positive,negative,even or odd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+int n;
+printf("enter the value of n: ");
+scanf("%d",&n);
+if (n > 0)
+{
+printf("The number %d is positive and ", n);
+}
+else if (n< 0)
+#include <stdio.h>
+void main()
+{
+int n;
+printf("enter the value of n: ");
+scanf("%d",&n);
+if (n > 0)
+{
+printf("The number %d is positive and ", n);
+}
+else if (n< 0)
